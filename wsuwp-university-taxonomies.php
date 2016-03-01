33a57c7d9b25fcb727129485e8d7fe57e8b231f6
jeremyfelt <jeremy.felt@gmail.com>
2016-03-01 14:04:49 -0800

    Bump version 0.3.2

--- wsuwp-university-taxonomies.php
+++ wsuwp-university-taxonomies.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: WSUWP University Taxonomies
-Version: 0.3.0
+Version: 0.3.2
 Plugin URI: https://web.wsu.edu/
 Description: Provides Washington State University taxonomies to WordPress
 Author: washingtonstateuniversity, jeremyfelt
