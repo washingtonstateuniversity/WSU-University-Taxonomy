c6ccb51321fbba78bedbb5efa82413b8e88f766c
Phil Cable <pcable@wsu.edu>
2017-09-26 17:08:03 -0700

    Remove the `closeOnSelect` option
    The default is `true`, which is the behavior we actually want.

--- js/edit-post.js
+++ js/edit-post.js
@@ -1,9 +1,8 @@
-( function( $, window ) {
+( function( $ ) {
 
 	// Initialize Select2.
 	$( ".taxonomy-select2" ).select2( {
 		placeholder: "+ Add",
-		closeOnSelect: false,
 		templateResult: function( data, container ) {
 			if ( data.element ) {
 				$( container ).addClass( $( data.element ).attr( "class" ) );
@@ -11,9 +10,4 @@
 			return data.text;
 		}
 	} );
-
-	// Re-render the Select2 dropdown position when a new selection is made.
-	$( ".taxonomy-select2" ).on( "change", function() {
-		$( window ).scroll();
-	} );
-}( jQuery, window ) );
+}( jQuery ) );
