5b09823f2af361394fab60de38f2a62cb962a4a2
Jeremy Felt <jeremy.felt@gmail.com>
2018-05-23 11:13:38 -0700

    Fix code styling

--- wsuwp-university-taxonomies.php
+++ wsuwp-university-taxonomies.php
@@ -73,7 +73,7 @@ class WSUWP_University_Taxonomies {
 		if ( get_option( 'wsu_taxonomy_schema', false ) !== $this->taxonomy_schema_version ) {
 			// Don't schedule a duplicate event if one has already been scheduled.
 			$next = wp_next_scheduled( $hook, $args );
-			if ( $next )  {
+			if ( $next ) {
 				return;
 			}
 
