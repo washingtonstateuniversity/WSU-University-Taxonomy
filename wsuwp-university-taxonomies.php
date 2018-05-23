0d4c0ad51f1b053dc99ab4c7128c63385dfdec91
Jeremy Felt <jeremy.felt@gmail.com>
2018-05-23 11:23:15 -0700

    Provide actual arguments when checking next scheduled

--- wsuwp-university-taxonomies.php
+++ wsuwp-university-taxonomies.php
@@ -72,7 +72,7 @@ class WSUWP_University_Taxonomies {
 	public function check_schema() {
 		if ( get_option( 'wsu_taxonomy_schema', false ) !== $this->taxonomy_schema_version ) {
 			// Don't schedule a duplicate event if one has already been scheduled.
-			$next = wp_next_scheduled( $hook, $args );
+			$next = wp_next_scheduled( 'wsu_taxonomy_update_schema', array() );
 			if ( $next ) {
 				return;
 			}
