d5d6a6f1b5a0eeaeaf974829e02ee0f75597aca4
Phil Cable <pcable@wsu.edu>
2018-08-20 16:24:23 -0700

    Bump version 0.4.10

--- wsuwp-university-taxonomies.php
+++ wsuwp-university-taxonomies.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: WSUWP University Taxonomies
-Version: 0.4.9
+Version: 0.4.10
 Plugin URI: https://web.wsu.edu/
 Description: Provides Washington State University taxonomies to WordPress
 Author: washingtonstateuniversity, jeremyfelt, philcable
