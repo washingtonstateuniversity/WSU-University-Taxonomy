5274011467ab48758d3003cb1afbf82ae5d7d171
Phil Cable <pcable@wsu.edu>
2018-09-10 09:54:42 -0700

    Update the schema version

--- wsuwp-university-taxonomies.php
+++ wsuwp-university-taxonomies.php
@@ -16,7 +16,7 @@ class WSUWP_University_Taxonomies {
 	 *
 	 * @var string Current version of the taxonomy schema.
 	 */
-	var $taxonomy_schema_version = '20180821-001';
+	var $taxonomy_schema_version = '20180910-001';
 
 	/**
 	 * @var string Taxonomy slug for the WSU University Category taxonomy.
