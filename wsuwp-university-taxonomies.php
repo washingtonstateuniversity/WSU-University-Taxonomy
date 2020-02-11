479a805a2384a6eb6109741f6fb5b7ca0e1859af
danbleile <danbleile@gmail.com>
2020-02-11 15:39:25 -0800

    Bump Version

--- wsuwp-university-taxonomies.php
+++ wsuwp-university-taxonomies.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: WSUWP University Taxonomies
-Version: 1.0.2
+Version: 1.0.3
 Plugin URI: https://web.wsu.edu/
 Description: Provides Washington State University taxonomies to WordPress
 Author: washingtonstateuniversity, jeremyfelt, philcable
